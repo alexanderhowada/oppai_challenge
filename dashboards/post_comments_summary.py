e5fcf8f6bd6ac4130d3cfc760df38050b74122ed
alexanderhowada <alexanderhowada>
2023-11-25 06:44:37 +0000

    changed to plotly for large datasets

---
+++ dashboards/post_comments_summary.py
@@ -0,0 +1,147 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC # Widgets
+
+# COMMAND ----------
+
+from datetime import datetime, timedelta
+from pyspark.sql import functions as F
+from utils.databricks import Widgets
+from machine_learning.linear_trend import LinearTrend
+from machine_learning.openai import OpenAIRequests
+
+# COMMAND ----------
+
+# MAGIC %run ./../etl/etl_constants
+
+# COMMAND ----------
+
+w = Widgets(dbutils)
+w.remove_all()
+
+# COMMAND ----------
+
+post_id_df = spark.sql(f"""
+SELECT DISTINCT
+    id_oid, created_at_date
+    , CASE WHEN translations_description IS NOT NULL AND translations_description != ''
+        THEN translations_description
+        ELSE translations_title
+    END translation_en
+FROM {TARGET_POSTS_ENGLISH_SILVER}
+WHERE created_at_date BETWEEN '{w("start_date")}' AND '{w("end_date")}'
+ORDER BY created_at_date DESC
+""").persist()
+
+post_ids = post_id_df.select('id_oid').toPandas()['id_oid'].tolist()
+w.create_text('post_id', post_ids[0])
+
+# COMMAND ----------
+
+def get_comments_from_post():
+    return spark.sql(f"""
+    SELECT DISTINCT
+        CASE WHEN translations_description IS NOT NULL AND translations_description != ''
+            THEN translations_description
+            ELSE translations_title
+        END body
+    FROM {TARGET_POSTS_ENGLISH_SILVER}
+    WHERE id_oid = '{w("post_id")}'
+
+    UNION ALL
+
+    SELECT DISTINCT body FROM {TARGET_POST_COMMENTS_JOIN_SILVER}
+    WHERE post_id_oid = '{w("post_id")}'
+    """).toPandas()['body'].tolist()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC # Summary of post comments.
+# MAGIC
+# MAGIC This script uses ChatGPT to summarize post comments without the need of reading them.
+# MAGIC
+# MAGIC To use this script:
+# MAGIC
+# MAGIC 1. choose a post by its id (id_oid).
+# MAGIC     - For demonstration purpose, I recommend choosing a post with a large ammount of post comments (see table with count of votes).
+# MAGIC     - Otherwise, choose any other (see table with posts sorted by date)
+# MAGIC 2. Paste the post_id in the widget post_id.
+# MAGIC 3. Run the script.
+# MAGIC 4. Read the summary.
+# MAGIC
+# MAGIC Observations:
+# MAGIC 1. ChatGPT can return different responses to the same statement, hence it may be interestint to run the script many times for the same post_id.
+# MAGIC 2. This uses the paid version of ChatGPT, therefore, please, be carefull with its use.
+# MAGIC     - The sum of total cost is capped at 5 USD.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC # Posts ordered by creation date.
+
+# COMMAND ----------
+
+post_id_df.display()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC # Posts ordered by comment count.
+
+# COMMAND ----------
+
+# print(comments)
+
+print("Comments with the most ammount of votes.")
+spark.sql(f"""
+SELECT post_id_oid, count(vote_id_oid) AS n_comments FROM {TARGET_POST_VOTES_JOIN_SILVER}
+GROUP BY 1
+ORDER BY 2 DESC
+""").display()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC # ChatGPT summary.
+
+# COMMAND ----------
+
+comments = get_comments_from_post()
+
+message = """Summarize these responses the message and write the main keywords.""" \
+    """For context, these messages were written in a Patreon for adult games\n""" \
+    """This is the message\n""" \
+    """------------------------------\n""" \
+    f"""{comments[0]}\n""" \
+    """------------------------------\n""" \
+    """And these are the responses\n""" \
+    """------------------------------\n""" \
+    f"""{str(comments[1:])}\n""" \
+    """------------------------------\n""" \
+    """Return a single JSON in the format: {"summary": "summary here", "keywords": ["keyword 1", "keyword 2", ...]}.""" \
+    """The JSON must contain only information regarding the responses."""
+
+print("------------post------------")
+print(comments[0])
+print("----------------------------")
+
+if len(comments) >= 5:
+    api = OpenAIRequests(dbutils.secrets.get(scope='openai', key='openai_key'))
+    r = api.chat_completion(message)
+    j = r.json()
+    df = api.get_content_df()
+    print(f"Total cost (USD): {api.calculate_cost()}")
+    df.display()
+else:
+    print("Less than 5 comments. Printing them.")
+    for c in comments:
+        print('--------------------')
+        print(c)
+        print('--------------------')
+
--- dashboards/users.py
+++ dashboards/users.py
@@ -1,4 +1,31 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC # Widgets
+
+# COMMAND ----------
+
+!pip3 install -U kaleido
+
+# COMMAND ----------
+
+import ipywidgets
+import plotly.graph_objects as go
+
+from utils.databricks import Widgets
+
+# COMMAND ----------
+
+w = Widgets(dbutils)
+w.remove_all()
+
+# COMMAND ----------
+
+w.create_text('per_country_order', 'n_users')
+w.create_text('n_bins', '50')
+
+# COMMAND ----------
+
 # MAGIC %run ../etl/etl_constants
 
 # COMMAND ----------
@@ -6,6 +33,17 @@
 # MAGIC %md
 # MAGIC
 # MAGIC ## Totals
+# MAGIC
+# MAGIC List users totals
+# MAGIC
+# MAGIC 1. 55k total users
+# MAGIC 1. 5060 patrons
+# MAGIC 1. 9.1% of the users are patrons
+# MAGIC 1. almost 600k raised for the campaign
+# MAGIC 1. 300k raised
+# MAGIC     - I am not sure about these results, since there is more money raised in the campaign than in the lifetime.
+# MAGIC 1. Only 19 followers (strange)
+# MAGIC 1. no free trial users
 
 # COMMAND ----------
 
@@ -24,7 +62,81 @@ FROM {TARGET_USERS_BRONZE_TB}""").display()
 
 # MAGIC %md
 # MAGIC
-# MAGIC ## Per country
+# MAGIC ## Support distribution per user
+# MAGIC
+# MAGIC ## Widgets:
+# MAGIC
+# MAGIC 1. per_country_order: the column to sort. See availables in the table tab.
+# MAGIC
+# MAGIC ## Plots
+# MAGIC
+# MAGIC These plots aims to analyze the distribution os monetary contribution per user and patron.
+# MAGIC
+# MAGIC - campaign_support: distribution of campaign lifetime support.
+# MAGIC - lifetime_support: distribution of lifetime support.
+# MAGIC - is_patron: share os patrons
+# MAGIC - patron support: share os patron suport vs non patrons.
+# MAGIC
+# MAGIC ## Analysis
+# MAGIC
+# MAGIC From the campaign and lifetime support tabs:
+# MAGIC
+# MAGIC 1. Almost 1800k of the users have contributed with 0-12 USD.
+# MAGIC 1. Contributions higher than 12 USD seems to decay exponentially fast.
+# MAGIC
+# MAGIC
+
+# COMMAND ----------
+
+help(go.Histogram)
+
+# COMMAND ----------
+
+def update_histogram(df):
+    fig = go.Figure()
+    fig.add_trace(
+        go.Histogram(x=df['campaign_support'], nbinsx=int(w('n_bins')), name='campaign_support')
+    )
+    fig.add_trace(
+        go.Histogram(x=df['lifetime_support'], nbinsx=int(w('n_bins')), name='lifetime_support')
+    )
+    fig.update_layout(title=f"Freq")
+    fig.update_yaxes(type='log', title="Freq")
+    fig.update_xaxes(type='linear', title="USD")
+    
+    fig.show()
+
+def share_per_user(df):
+    fig = go.Figure()
+    df = df.groupby('is_patron').sum(['campaign_support', 'lifetime_support']).reset_index()
+    fig.add_trace(
+        go.Pie(labels=df['is_patron'], values=df['campaign_support'])
+    )
+    fig.update_layout(
+        title="Share of campaign_support",
+        legend={"title": "is_patron"}
+    )
+    fig.show()
+    
+
+df = spark.sql(f"""
+SELECT
+    patreon_status_campaign_lifetime_support_cents/100 AS campaign_support
+    , patreon_status_lifetime_support_cents/100 as lifetime_support
+    , COALESCE(patreon_status_is_patron, False) AS is_patron
+FROM {TARGET_USERS_BRONZE_TB}
+""").toPandas()
+
+update_histogram(df)
+share_per_user(df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC ## Per country distribution
+# MAGIC
+# MAGIC Users totals per country
 
 # COMMAND ----------
 
@@ -39,30 +151,14 @@ SELECT
     , SUM(CAST(patreon_status_is_free_trial AS BIGINT)) AS is_free_trial
 FROM {TARGET_USERS_BRONZE_TB}
 GROUP BY 1
-ORDER BY 3 DESC
+ORDER BY {w('per_country_order')} DESC
 """).display()
 
 # COMMAND ----------
 
 # MAGIC %md
 # MAGIC
-# MAGIC ## Distribution
-
-# COMMAND ----------
-
-spark.sql(f"""
-SELECT
-    patreon_status_campaign_lifetime_support_cents/100 AS campaign_support
-    , patreon_status_lifetime_support_cents/100 as lifetime_support
-    , COALESCE(patreon_status_is_patron, False) AS is_patron
-FROM {TARGET_USERS_BRONZE_TB}
-""").display()
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC
-# MAGIC ### per country and patron
+# MAGIC # Per country and patron
 
 # COMMAND ----------
 
@@ -72,6 +168,10 @@ SELECT
     , COALESCE(patreon_status_is_patron, FALSE) as is_patron
     , SUM(patreon_status_campaign_lifetime_support_cents)/100 AS campaign_support
 FROM {TARGET_USERS_BRONZE_TB}
-GROUP BY 1, 2
-ORDER BY campaign_support, is_patron, country DESC
+GROUP BY ALL
+ORDER BY campaign_support DESC
 """).display()
+
+# COMMAND ----------
+
+
