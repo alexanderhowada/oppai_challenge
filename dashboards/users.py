a0992c0ad3890969d9af5d90bdfcd86b0999c419
alexanderhowada <alexanderhowada>
2023-11-26 21:33:54 +0000

    fixed users

--- dashboards/users.py
+++ dashboards/users.py
@@ -32,7 +32,7 @@ w.create_text('n_bins', '50')
 
 # MAGIC %md
 # MAGIC
-# MAGIC ## Totals
+# MAGIC # Totals
 # MAGIC
 # MAGIC List users totals
 # MAGIC
@@ -62,7 +62,7 @@ FROM {TARGET_USERS_BRONZE_TB}""").display()
 
 # MAGIC %md
 # MAGIC
-# MAGIC ## Support distribution per user
+# MAGIC # Support distribution per user
 # MAGIC
 # MAGIC ## Widgets:
 # MAGIC
@@ -71,28 +71,55 @@ FROM {TARGET_USERS_BRONZE_TB}""").display()
 # MAGIC ## Plots
 # MAGIC
 # MAGIC These plots aims to analyze the distribution os monetary contribution per user and patron.
+# MAGIC The plots are made using Plotly due to limitations of the plots in Databricks notebooks.
 # MAGIC
-# MAGIC - campaign_support: distribution of campaign lifetime support.
-# MAGIC - lifetime_support: distribution of lifetime support.
-# MAGIC - is_patron: share os patrons
-# MAGIC - patron support: share os patron suport vs non patrons.
+# MAGIC - share of user supporters: share of users that have monetary contribution > 0.
+# MAGIC - Share of campaign support: Share of campaign support contribution per user patron.
+# MAGIC - campaign share: distribution of campaign lifetime support (excludes non contributors).
+# MAGIC - top supporters: table with the top supporters
 # MAGIC
 # MAGIC ## Analysis
 # MAGIC
-# MAGIC From the campaign and lifetime support tabs:
+# MAGIC ### share of user supporters
 # MAGIC
-# MAGIC 1. Almost 1800k of the users have contributed with 0-12 USD.
-# MAGIC 1. Contributions higher than 12 USD seems to decay exponentially fast.
+# MAGIC 1. Almost 70% of the userbase have contributed in the past.
 # MAGIC
+# MAGIC ### share of campaign support
 # MAGIC
+# MAGIC 1. Almost 60% of the total monetary contribution are from patrons.
+# MAGIC 2. However, a lot (40%) is from non patrons.
+# MAGIC
+# MAGIC ### Support distribution (excludes non contributors)
+# MAGIC
+# MAGIC 1. About 11k users have donated less than 20 USD.
+# MAGIC 1. The outliers have donated less than 1k USD, therefore they are not extreme outliers and cannot be considered "angel investors".
+# MAGIC 1. The linear decline (on a log scale), seems to indicate that the probabilty of monetary donation decays exponentially fast.
+# MAGIC     > This may indicate that extreme (rare events) statistcs may be ideal to describe users monetary contribution.
+# MAGIC
+# MAGIC     > This kind of statiscs should consider that the tail of the probability distribution is equally important as its bulk if we are considering monetary contribution.
+# MAGIC
+# MAGIC ### Top 10 supporters
+# MAGIC
+# MAGIC 1. None of the supporters have outstanding donations.
+# MAGIC 1. All of them are from developed countries and speak english.
 
 # COMMAND ----------
 
-help(go.Histogram)
+def share_contributors(df):
+    fig = go.Figure()
+    df['supporter'] = df['campaign_support'].apply(lambda x: True if x > 0 else False)
+    fig.add_trace(
+        go.Pie(labels=df['supporter'])
+    )
+    fig.update_layout(
+        title="Share of user supporters",
+        legend={"title": "User has monetary contribution"},
+    )
+    fig.show()
 
-# COMMAND ----------
-
-def update_histogram(df):
+def monetary_support(df, exclude_0=True):
+    if exclude_0:
+        df = df[df['campaign_support'] > 0]
     fig = go.Figure()
     fig.add_trace(
         go.Histogram(x=df['campaign_support'], nbinsx=int(w('n_bins')), name='campaign_support')
@@ -100,13 +127,13 @@ def update_histogram(df):
     fig.add_trace(
         go.Histogram(x=df['lifetime_support'], nbinsx=int(w('n_bins')), name='lifetime_support')
     )
-    fig.update_layout(title=f"Freq")
+    fig.update_layout(title=f"support distribution (exclude 0 support)")
     fig.update_yaxes(type='log', title="Freq")
     fig.update_xaxes(type='linear', title="USD")
     
     fig.show()
 
-def share_per_user(df):
+def share_of_patron_support(df):
     fig = go.Figure()
     df = df.groupby('is_patron').sum(['campaign_support', 'lifetime_support']).reset_index()
     fig.add_trace(
@@ -117,26 +144,65 @@ def share_per_user(df):
         legend={"title": "is_patron"}
     )
     fig.show()
-    
 
 df = spark.sql(f"""
 SELECT
-    patreon_status_campaign_lifetime_support_cents/100 AS campaign_support
-    , patreon_status_lifetime_support_cents/100 as lifetime_support
+    COALESCE(patreon_status_campaign_lifetime_support_cents, 0)/100 AS campaign_support
+    , COALESCE(patreon_status_lifetime_support_cents, 0)/100 as lifetime_support
     , COALESCE(patreon_status_is_patron, False) AS is_patron
 FROM {TARGET_USERS_BRONZE_TB}
 """).toPandas()
 
-update_histogram(df)
-share_per_user(df)
+share_contributors(df)
+share_of_patron_support(df)
+monetary_support(df)
+
+print("top 10 supporters")
+df = spark.sql(f"""
+SELECT
+    id_oid
+    , country
+    , language
+    , COALESCE(patreon_status_campaign_lifetime_support_cents, 0)/100 AS campaign_support
+    , COALESCE(patreon_status_lifetime_support_cents, 0)/100 as lifetime_support
+FROM {TARGET_USERS_BRONZE_TB}
+ORDER BY patreon_status_campaign_lifetime_support_cents DESC
+LIMIT 10
+""").display()
 
 # COMMAND ----------
 
 # MAGIC %md
 # MAGIC
-# MAGIC ## Per country distribution
+# MAGIC # Per country distribution
 # MAGIC
-# MAGIC Users totals per country
+# MAGIC ## Plots
+# MAGIC
+# MAGIC - n_users: distribution of users per country
+# MAGIC - user_share: share of users per country
+# MAGIC - lifetime_support: contribution distribution
+# MAGIC - share_lifetime_support: share of support per country
+# MAGIC
+# MAGIC ## n_users
+# MAGIC
+# MAGIC - If we set the widget per_country_order to n_users,
+# MAGIC   the distribution shows that there are two classes of countries:
+# MAGIC   > The first few (up to FR), have a unnusual high number of users if compared with the remaining.
+# MAGIC
+# MAGIC   > The other countries seems to follow an exponential distribution (linear decay on a log scale).
+# MAGIC
+# MAGIC - The number of patrons is not proportiornal to the number of users.
+# MAGIC
+# MAGIC ## user_share
+# MAGIC
+# MAGIC - The user share plot seems to illustrate very well that the top countries have the largest share (abount 70% up to FR)
+# MAGIC
+# MAGIC ## lifetime_support
+# MAGIC
+# MAGIC - sorting by n_users shows that number of users does not directly correlate with monetary contribution.
+# MAGIC - sorting by lifetime_support we can see that the ranking of countries changes, further emphasizing that number of users does not directly correlate with money.
+# MAGIC - It is interesting to note that a few countries (BR, for example) have small monetary contribution (0.94%) despite having a large (4.19%) user base.
+# MAGIC - It is also interesting to notice that the distribution of monetary distribution decays much faster then the user base. This may be an opportunity to localize Patreon contributions in order to reach a larger user base.
 
 # COMMAND ----------
 
@@ -164,7 +230,7 @@ ORDER BY {w('per_country_order')} DESC
 
 spark.sql(f"""
 SELECT
-    country
+    COALESCE(country, 'None') AS country
     , COALESCE(patreon_status_is_patron, FALSE) as is_patron
     , SUM(patreon_status_campaign_lifetime_support_cents)/100 AS campaign_support
 FROM {TARGET_USERS_BRONZE_TB}
